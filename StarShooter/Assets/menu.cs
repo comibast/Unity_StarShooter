7b3a3d361ab4769d33cef77348cc09031a1ca3e8
comibast <105775403+comibast@users.noreply.github.com>
2022-07-12 16:28:37 +0800

    Update：開始遊戲鈕，離開遊戲鈕，設定鈕，回到遊戲鈕。

--- StarShooter/Assets/menu.cs
+++ StarShooter/Assets/menu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
 {
@@ -15,4 +16,24 @@ public class menu : MonoBehaviour
     {
         
     }
+
+    public void NextScene()
+    {
+        //舊式換場景:(玩家要等加載)
+        //Application.LoadLevel("下一個場景名稱");
+        //Application.LoadLevel(ID);
+        //新式換場景:(預先加載,玩家不用等)
+        //SceneManager.LoadScene("下一個場景名稱");
+        //SceneManager.LoadScene(ID);
+        SceneManager.LoadScene("level");
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    
+
+
 }
